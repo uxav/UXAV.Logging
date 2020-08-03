1040917dda0d7a4145c2c599717a3c2844d5aab5
Mike Jobson <mike@ux.digital>
2020-08-03 19:11:20 +0100

    Tidy up

--- .github/scripts/GenerateVersionNumber.ps1
+++ .github/scripts/GenerateVersionNumber.ps1
@@ -1,5 +1,5 @@
 param([string] $Path = "" )
 $dll = Get-Item -Path $Path
 $version = [version]$dll.VersionInfo.ProductVersion
-$versionString = "v{0}.{1}.{2}" -f $version.Major, $version.Minor, $version.Build
+$versionString = "{0}.{1}.{2}" -f $version.Major, $version.Minor, $version.Build
 Write-Output $versionString
