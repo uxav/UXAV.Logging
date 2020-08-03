5d8e238172ff031d3ca124be36d6e737b972e5ed
Mike Jobson <mike@ux.digital>
2020-08-03 19:15:54 +0100

    Develop (#5)
    * Changed workspace references path
    
    * Updated packages
    
    * Updated workflow
    
    * Updated workflow
    
    * Updated workflow
    
    * Updated workflow
    
    * Updated workflow
    
    * Updated workflow
    
    * Updated workflow
    
    * Updated workflow
    
    * Updated workflow
    
    * Updated workflow
    
    * Updated workflow
    
    * Updated workflow
    
    * Updated workflow
    
    * Tidy up
    
    * Workflow fix

---
+++ .github/scripts/GenerateVersionNumber.ps1
@@ -0,0 +1,5 @@
+param([string] $Path = "" )
+$dll = Get-Item -Path $Path
+$version = [version]$dll.VersionInfo.ProductVersion
+$versionString = "{0}.{1}.{2}" -f $version.Major, $version.Minor, $version.Build
+Write-Output $versionString
