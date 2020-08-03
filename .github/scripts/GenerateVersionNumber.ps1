0191227fc499c0c25279747e60c235102e31d5cb
Mike Jobson <mike@ux.digital>
2020-08-03 18:10:38 +0100

    Updated workflow

--- .github/scripts/GenerateVersionNumber.ps1
+++ .github/scripts/GenerateVersionNumber.ps1
@@ -1,4 +1,4 @@
 param([string] $path = "" )
 $version = (Get-Item UXAV.Logging/bin/Release/UXAV.Logging.dll).VersionInfo.ProductVersion
-$version = "v{0}.{1}.{2}" -f $version.Major, $version.Minor, $version.Build
-Write-Output $version
+$versionString = "v{0}.{1}.{2}" -f $version.Major, $version.Minor, $version.Build
+Write-Output $versionString
