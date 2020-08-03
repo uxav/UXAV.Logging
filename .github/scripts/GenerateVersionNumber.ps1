0721ddd55854fb1d053c93afe1ae34ded38e6e06
Mike Jobson <mike@ux.digital>
2020-08-03 17:55:43 +0100

    Updated workflow

--- .github/scripts/GenerateVersionNumber.ps1
+++ .github/scripts/GenerateVersionNumber.ps1
@@ -1,4 +1,4 @@
 param([string] $path = "" )
-$version = (Get-Item PaymentSenseAV/bin/Release/PaymentSenseAV.dll).VersionInfo.ProductVersion
+$version = (Get-Item UXAV.Logging/bin/Release/UXAV.Logging.dll).VersionInfo.ProductVersion
 $version = "v{0}.{1}.{2}" -f $version.Major, $version.Minor, $version.Build
 Write-Output $version
