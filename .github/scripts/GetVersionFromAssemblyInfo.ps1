f3714fc41d7d9b5331af72f8511c2ac3c23b2f3f
Mike Jobson <mike@ux.digital>
2024-03-09 15:08:30 +0000

    Modified workflows for actions

--- .github/scripts/GetVersionFromAssemblyInfo.ps1
+++
@@ -1,6 +0,0 @@
-param([string] $Path)
-
-$file = Get-Item $Path;
-$fileContent = Get-Content $file.FullName
-$version = [Regex]::Match($fileContent, '(?<!// *)\[assembly: AssemblyVersion\("(\d+\.\d+\.\d+).*"\)\]').Captures.Groups[1].Value
-Write-Output $version
